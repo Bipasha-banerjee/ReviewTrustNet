dc58369e1575727822873681d488672b1fec1b01
Bipasha-banerjee <bips.ban@gmail.com>
2019-04-21 16:10:46 -0400

    modified class NodeInfo.java

--- src/edu/vt/NetInf/NodeInfo.java
+++ src/edu/vt/NetInf/NodeInfo.java
@@ -1,4 +1,15 @@
 package edu.vt.NetInf;
 
 public class NodeInfo {
+    private String NId;
+    private Integer CascadeNumber;
+
+    public NodeInfo()
+    { }
+    public NodeInfo(String NodeName, Integer Volume){
+        NId = NodeName;
+        CascadeNumber = Volume;
+    }
+
+    // Stream In and Stream Out have been left out.
 }
